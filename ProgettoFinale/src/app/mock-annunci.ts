fd1fd29eb86d7fd4fccf47e1f734b1b40199e483
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2019-06-05 12:53:02 +0000

    progettofinito

--- ProgettoFinale/src/app/mock-annunci.ts
+++ ProgettoFinale/src/app/mock-annunci.ts
@@ -1,8 +1,8 @@
-    
+
 import {Annuncio} from './annuncio';
 
 export const ANNUNCI : Annuncio[] = [
-    {nome : 'Giacomo', cognome : 'Poretti', prodotto : 'Macchina', descrizione : 'blu e coi sedili in pelle di fagiano', commento : ''},
-    {nome : 'Giovanni', cognome : 'Storti', prodotto : 'Iphone X', descrizione : 'rubato ad un marocchino in spiaggia', commento : ''},
-    {nome : 'Aldo', cognome : 'Baglio', prodotto : 'Peperonata', descrizione : 'ottima alle 6 del mattino', commento : ''}
+    {nome : 'marco', cognome : 'corrado', prodotto : 'Macchina', descrizione : 'blu e coi sedili in pelle di fagiano', commento : ''},
+    {nome : 'dome', cognome : 'bisness', prodotto : 'Iphone X', descrizione : 'rubato ad un marocchino in spiaggia', commento : ''},
+    {nome : 'Dio', cognome : 'Dio', prodotto : 'Peperonata', descrizione : 'ottima alle 6 del mattino', commento : ''}
   ];
